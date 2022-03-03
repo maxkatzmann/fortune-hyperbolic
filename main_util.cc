b687774d88b5665ab25a7d350965cc0353e82d6e
Maximilian Katzmann <maximilian.katzmann@hpi.de>
2022-03-03 15:02:39 +0100

    Allow for easier specification of utilized precision.

--- main_util.cc
+++ main_util.cc
@@ -16,9 +16,21 @@ using namespace hyperbolic;
 using namespace boost;
 using namespace multiprecision;
 
-// use 200 bit floating point numbers
-using floating_point_type = number<mpfr_float_backend<200, allocate_stack>>;
-//using floating_point_type = double;
+using floating_point_type_32 = number<mpfr_float_backend<32, allocate_stack>>;
+using floating_point_type_48 = number<mpfr_float_backend<48, allocate_stack>>;
+using floating_point_type_64 = number<mpfr_float_backend<64, allocate_stack>>;
+using floating_point_type_80 = number<mpfr_float_backend<80, allocate_stack>>;
+using floating_point_type_96 = number<mpfr_float_backend<96, allocate_stack>>;
+using floating_point_type_112 = number<mpfr_float_backend<112, allocate_stack>>;
+using floating_point_type_128 = number<mpfr_float_backend<128, allocate_stack>>;
+using floating_point_type_144 = number<mpfr_float_backend<144, allocate_stack>>;
+using floating_point_type_160 = number<mpfr_float_backend<160, allocate_stack>>;
+using floating_point_type_176 = number<mpfr_float_backend<176, allocate_stack>>;
+using floating_point_type_192 = number<mpfr_float_backend<192, allocate_stack>>;
+using floating_point_type_208 = number<mpfr_float_backend<208, allocate_stack>>;
+using floating_point_type_224 = number<mpfr_float_backend<224, allocate_stack>>;
+using floating_point_type_240 = number<mpfr_float_backend<240, allocate_stack>>;
+using floating_point_type_256 = number<mpfr_float_backend<256, allocate_stack>>;
 
 int main(int argc, char* argv[]) {
 
@@ -30,6 +42,7 @@ int main(int argc, char* argv[]) {
             ("v,verbose", "Enable verbose output (only for debugging)", cxxopts::value<bool>()->default_value("false"))
             ("d,output_diagram", "Output Filename for writing the diagram svg", cxxopts::value<std::string>())
             ("t,output_triangulation", "Output Filename for writing the delaunay triangulation", cxxopts::value<std::string>())
+            ("p,precision", "Specifies the number of bits that should be used for computations.  Allowed values are multiple of 16 in [32, ..., 256].  Defaults to Double presision for values outside of that range.", cxxopts::value<int>()->default_value("0"))
             ("h,help", "Print usage");
 
     auto result = options.parse(argc, argv);
@@ -64,11 +77,109 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    int precision = result["p"].as<int>();
+
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
     VoronoiDiagram v;
-    FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type>, floating_point_type> fortune(v, sites, result["v"].as<bool>());
-    fortune.calculate();
+
+    switch (precision) {
+    case 32: {
+      cout << "Using a precision of 32bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_32>, floating_point_type_32> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 48: {
+      cout << "Using a precision of 48bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_48>, floating_point_type_48> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 64: {
+      cout << "Using a precision of 64bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_64>, floating_point_type_64> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 80: {
+      cout << "Using a precision of 80bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_80>, floating_point_type_80> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 96: {
+      cout << "Using a precision of 96bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_96>, floating_point_type_96> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 112: {
+      cout << "Using a precision of 112bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_112>, floating_point_type_112> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 128: {
+      cout << "Using a precision of 128bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_128>, floating_point_type_128> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 144: {
+      cout << "Using a precision of 144bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_144>, floating_point_type_144> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 160: {
+      cout << "Using a precision of 160bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_160>, floating_point_type_160> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 176: {
+      cout << "Using a precision of 176bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_176>, floating_point_type_176> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 192: {
+      cout << "Using a precision of 192bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_192>, floating_point_type_192> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 208: {
+      cout << "Using a precision of 208bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_208>, floating_point_type_208> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 224: {
+      cout << "Using a precision of 224bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_224>, floating_point_type_224> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 240: {
+      cout << "Using a precision of 240bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_240>, floating_point_type_240> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    case 256: {
+      cout << "Using a precision of 256bits.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<floating_point_type_256>, floating_point_type_256> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+      break;
+    }
+    default: {
+      cout << "Using Double precision.\n";
+      FortuneHyperbolicImplementation<FullNativeKernel<double>, double> fortune(v, sites, result["v"].as<bool>());
+      fortune.calculate();
+    }
+    }
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
